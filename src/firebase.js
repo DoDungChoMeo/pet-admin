c8f955c5bd268b416321766cbeb71d750f988be6
Lee TB <tblyduc2412@gmail.com>
2022-10-06 20:01:49 +0700

    setup: add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+// connect to development environment
+if (
+  window.location.hostname === '127.0.0.1' ||
+  window.location.hostname === 'localhost'
+) {
+  connectFirestoreEmulator(firestore, 'localhost', 8080);
+}
+
+export { analytics, firestore };
+export default app;

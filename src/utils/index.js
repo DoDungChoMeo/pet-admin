e344c7cfa36237c42be53d0bde1a594be2a6b760
Lee TB <tblyduc2412@gmail.com>
2022-10-07 16:07:46 +0700

    chore: random color utils

---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export { default as removeVietnameseTones } from './removeVietnameseTones';
+export { default as toKebabCase } from './toKebabCase';
+export { default as randomColor } from './randomColor';

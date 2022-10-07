513f99fe964acfd8e7d131e8f1f7cb514602da9b
Lee TB <tblyduc2412@gmail.com>
2022-10-07 16:12:13 +0700

    refactor: components to screens product

--- src/components/AddBrand.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AddBrand() {
-  return <div>AddBrand</div>;
-}
-
-export default AddBrand;
--- src/components/ProductsTable.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Table } from 'antd';
-
-function ProductsTable() {
-  return <div></div>;
-}
-
-export default ProductsTable;

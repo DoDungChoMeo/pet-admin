d5040c905f0eb9f0caa9a2341dc2317a8929faab
Lee TB <tblyduc2412@gmail.com>
2022-10-07 16:10:08 +0700

    feat: add reusable component Spin

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,1 @@
-export { default as AddProductForm } from './AddProductForm';
-export { default as AddBrand } from './AddBrand';
-export { default as AddCategory } from './AddCategory';
-export { default as ProductsTable } from './ProductsTable';
+export { default as Spin } from './Spin/Spin';

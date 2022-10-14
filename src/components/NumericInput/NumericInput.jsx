5c1d6f15c7c59e5d5664917f5f0a664a19c83add
Lee TB <tblyduc2412@gmail.com>
2022-10-14 16:47:08 +0700

    feat: reusable NumericInput

---
+++ src/components/NumericInput/NumericInput.jsx
@@ -0,0 +1,25 @@
+import { Input } from 'antd';
+
+const NumericInput = (props) => {
+  const { value, onChange, min } = props;
+
+  const handleChange = (e) => {
+    const { value: inputValue } = e.target;
+    const reg = /^-?\d*(\.\d*)?$/;
+    if (reg.test(inputValue) || inputValue === '' || inputValue === '-') {
+      onChange(inputValue);
+    }
+  };
+
+  const handleBlur = () => {
+    let valueTemp = value;
+    if (value.charAt(value.length - 1) === '.' || value === '-') {
+      valueTemp = value.slice(0, -1);
+    }
+    onChange(valueTemp.replace(/0*(\d+)/, '$1'));
+  };
+
+  return <Input {...props} onChange={handleChange} onBlur={handleBlur} />;
+};
+
+export default NumericInput;

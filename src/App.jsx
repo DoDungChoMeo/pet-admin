783e176e9df825e76249e61914e7e9de313d84a0
Lee TB <tblyduc2412@gmail.com>
2022-10-16 22:04:53 +0700

    fix: home page from dynamiclist to allproduct component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from '~/layouts/MainLayout';
 import AllProductPage from '~/pages/AllProductPage';
 import AddProductPage from '~/pages/AddProductPage';
-import DynamicFormList from '~/screens/product/DynamicFormList';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<DynamicFormList />} />
+          <Route index element={<AllProductPage />} />
           <Route path="product">
             <Route path="list" element={<AllProductPage />} />
             <Route path="new" element={<AddProductPage />} />

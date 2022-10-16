aa2f5a8f8ef45635ceaf412a164d634bfd2c0ee5
Lee TB <tblyduc2412@gmail.com>
2022-10-16 16:34:55 +0700

    feat: product model

---
+++ models.js
@@ -0,0 +1,23 @@
+const productModel = {
+  productId: 'nem-hinh-chan-cho-cho-thu-cung--1665905905966',
+  title: 'Nệm Hình Chân Chó Cho Thú Cưng',
+  brand: 'Đang cập nhật',
+  description:
+    'Với nhiều người, chó mèo không chỉ là động vật giúp giữ nhà, đuổi chuột. Nó còn là người bạn nhỏ thân thiết và rất trung thành. Do đó, hãy thể hiện tình yêu thương của bạn dành cho “người bạn nhỏ” đáng yêu này bằng cách trang bị cho chúng một chiếc giường nhỏ ấm cúng và êm ái để thú cưng có một nơi thư giãn thật thoải mái. Sản phẩm Nệm Hình Chân Chó Cho Thú Cưng ra đời sẽ giúp bạn xử lý vấn đề này!',
+  images: [
+    'https://res.cloudinary.com/leetb/image/upload/v1665905417/pet-friends/chanchonau1.webp.webp',
+  ],
+  inventories: [
+    {
+      stock: 10,
+      price: 270000,
+      specs: [
+        { k: 'size', v: 'S' },
+        { k: 'color', v: 'Nâu' },
+        { k: 'material', v: 'Cotton' },
+      ],
+      createAt: { _methodName: 'serverTimestamp' },
+    },
+  ],
+  createAt: { _methodName: 'serverTimestamp' },
+};

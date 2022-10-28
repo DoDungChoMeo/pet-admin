30e5105a6cacf1e721202397cdca772f1d1d6af7
Lee TB <tblyduc2412@gmail.com>
2022-10-28 20:11:37 +0700

    chore: delete export addBrand, addCategory

--- src/screens/product/index.js
+++ src/screens/product/index.js
@@ -1,5 +1,3 @@
 export { default as AddProductForm } from './AddProductForm';
-export { default as AddBrand } from './AddBrand';
-export { default as AddCategory } from './AddCategory';
 export { default as AddSelectList } from './AddSelectList';
 export { default as ProductTable } from './ProductTable';

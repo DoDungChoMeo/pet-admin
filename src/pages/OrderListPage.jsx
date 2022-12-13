d50b5c23ae5ca057a2edbd7b4b6fc1304331ca43
Lee TB <tblyduc2412@gmail.com>
2022-12-14 00:32:00 +0700

    FEAT: classify orders into pending, processed, canceled

--- src/pages/OrderListPage.jsx
+++ src/pages/OrderListPage.jsx
@@ -1,22 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import { List, Typography } from 'antd';
+import { List, Typography, Tabs } from 'antd';
 
 import { Order } from '~/components';
 import useFirestoreQuery from '~/hooks/useFirestoreQuery/useFirestoreQuery';
-import { getFirestore, collection, query, orderBy } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  query,
+  orderBy,
+  where,
+} from 'firebase/firestore';
 
 const { Title } = Typography;
 
 function OrderListPage() {
+  const items = [
+    {
+      label: 'Đang chờ',
+      key: 'order-pending',
+      children: <OrderList status={'pending'} />,
+    },
+    {
+      label: 'Đã xử lý',
+      key: 'order-processed',
+      children: <OrderList status={'processed'} />,
+    },
+    {
+      label: 'Đã hủy',
+      key: 'order-canceled',
+      children: <OrderList status={'canceled'} />,
+    },
+  ];
+
+  return (<>
+    <ContainerStyled className="order-list-page">
+      <Tabs items={items} />
+    </ContainerStyled>
+  </>
+  );
+}
+
+function OrderList({ status }) {
   const firestore = getFirestore();
   const ordersRef = collection(firestore, 'orders');
-  const q = query(ordersRef, orderBy('createAt', 'desc'));
+  const q = query(
+    ordersRef,
+    where("status", "==", status),
+  );
   const [orders, ordersLoading] = useFirestoreQuery(q);
 
   return (
-    <ContainerStyled className="order-list-page">
-      <Title level={2}>Danh sách đơn đặt hàng</Title>
+    <>
+      <Title level={2}>{`Danh sách đơn đặt hàng ${status==="processed" && 'đã xử lý' || status==="canceled" && 'đã bị hủy' || status==="pending" && 'đang chờ' } (${orders.length})`}</Title>
       <List
         dataSource={orders}
         renderItem={(item) => (
@@ -25,7 +61,7 @@ function OrderListPage() {
           </List.Item>
         )}
       />
-    </ContainerStyled>
+    </>
   );
 }
 
